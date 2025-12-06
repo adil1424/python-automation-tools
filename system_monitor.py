469a1070e06545fe20f0746e8210f6a2ee2baa2f
Adil nisar <adil.nisar517@gmail.com>
2025-12-05 22:38:10 -0500

    Add system monitoring script

---
+++ system_monitor.py
@@ -0,0 +1,14 @@
+# system_monitor.py
+# CPU, memory, and disk monitor
+
+import psutil
+import time
+
+while True:
+    cpu = psutil.cpu_percent(interval=1)
+    mem = psutil.virtual_memory().percent
+    disk = psutil.disk_usage('/').percent
+
+    print(f"CPU: {cpu}% | Memory: {mem}% | Disk: {disk}%")
+
+    time.sleep(5)
